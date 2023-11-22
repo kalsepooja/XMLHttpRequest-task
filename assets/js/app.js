fe119597207f07fb66d39302d4b1d899d3fb1b59
kalse pooja <kalsepooja056@gmail.com>
2023-11-22 23:48:46 +0530

    work on post method

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
 const cl = console.log;
 const cardContainer = document.getElementById('cardContainer');
+const postForm = document.getElementById('postForm');
+const titleControl = document.getElementById('title');
+const bodyControl = document.getElementById('body');
+const userIdControl = document.getElementById('userId');
+
+
 
 let baseUrl = `https://jsonplaceholder.typicode.com`;
 
 let postUrl = `${baseUrl}/posts`;
+let postArray = [];
 
-const xhr = new XMLHttpRequest();
 
 const templating = (arr => {
     let result = '';
@@ -32,6 +38,40 @@ const templating = (arr => {
     cardContainer.innerHTML = result;
 })
 
+const onSubmitHandler = (eve) => {
+    eve.preventDefault();
+    let postObj = {
+        title: titleControl.value,
+        body: bodyControl.value,
+        userId: userIdControl.value
+    };
+    cl(postObj);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", postUrl, true);
+
+    xhr.send(JSON.stringify(postObj));
+
+    xhr.onload = function() {
+        if(xhr.status === 200 || xhr.status === 201){
+            cl(xhr.response)
+            postObj.id = JSON.parse(xhr.response).id;
+            postArray.push(postObj)
+            templating(postArray)
+        }
+    }
+    postForm.reset()
+}
+    
+    // onSubmitHandler()
+
+
+postForm.addEventListener("submit", onSubmitHandler)
+
+const onGetHander = () => {
+    const xhr = new XMLHttpRequest();
+
 xhr.open("Get", postUrl, true);
 
 xhr.send();
@@ -41,10 +81,13 @@ xhr.onload = function(){
 
     if(xhr.status === 200){
         cl(xhr.response)
-        let data = JSON.parse(xhr.response);
-        cl(data)
-        templating(data)
+        postArray = JSON.parse(xhr.response);
+        // cl(data)
+        templating(postArray)
     }else{
         cl("something went wrong")
     }
 }
+};
+
+onGetHander()
