2143564dfe47d2772c0bf32d61315ef6f4fe8636
kalse pooja <kalsepooja056@gmail.com>
2023-11-26 01:19:11 +0530

    functionality on update button and delete button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,8 @@ let postArray = [];
 const onEditHandler = (eve => {
     cl(eve)
     let getId = eve.closest(".card").id;
+    localStorage.setItem("editId", getId);
+  
     // cl(getId)
     let getUrl = `${baseUrl}/posts/${getId}`;
 
@@ -39,7 +41,60 @@ const onEditHandler = (eve => {
         addBtn.classList.add('d-none');
         UpdateBtn.classList.remove('d-none');
     }
-}) 
+});
+
+const onUpdateHandler = (eve => {
+    // cl(eve)
+    let updatedObj = {
+        title: titleControl.value,
+        body: bodyControl.value,
+        userId: userIdControl.value
+    }
+    let getEditId = localStorage.getItem("editId");
+    // cl(getEditId);
+    let getUrl = `${postUrl}/${getEditId}`;
+    // cl(getUrl)
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("PATCH", getUrl, true);
+
+    xhr.send(JSON.stringify(updatedObj));
+
+    xhr.onload = function(){
+        // cl(xhr.response)
+        if(xhr.status === 200){
+            let getUpdatedObj = postArray.findIndex(ele =>{
+                return ele.id == getEditId
+            });
+            // cl(getUpdatedObj)
+            postArray[getUpdatedObj].title = updatedObj.title;
+            postArray[getUpdatedObj].body = updatedObj.body;
+            postArray[getUpdatedObj].userId = updatedObj.userId;
+            templating(postArray)
+        }
+    }
+});
+
+const onDeletHandler = (eve => {
+    // cl(eve)
+    let getDeletId = eve.closest('.card').id;
+    cl(getDeletId);
+    let deletUrl = `${postUrl}/${getDeletId}`;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("DELETE", deletUrl, true);
+
+    xhr.send();
+
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            let getDelete = document.getElementById(getDeletId);
+            getDelete.remove();
+        }
+    }
+});
 
 const templating = (arr => {
     let result = '';
@@ -56,7 +111,7 @@ const templating = (arr => {
                 </div>
                 <div class="card-footer d-flex justify-content-between">
                     <button class="btn btn-primary" onclick= "onEditHandler(this)">Edit</button>
-                    <button class="btn btn-danger">Delete</button>
+                    <button class="btn btn-danger" onclick = "onDeletHandler(this)">Delete</button>
                 </div>
             </div>
         
@@ -64,7 +119,7 @@ const templating = (arr => {
                 `
     })
     cardContainer.innerHTML = result;
-})
+});
 
 const onSubmitHandler = (eve) => {
     eve.preventDefault();
@@ -117,7 +172,7 @@ const onGetHander = () => {
         }
 }
 };
-// UpdateBtn.addEventListener("click", onUpdateHandler)
+UpdateBtn.addEventListener("click", onUpdateHandler)
 postForm.addEventListener("submit", onSubmitHandler);
 
 onGetHander()
